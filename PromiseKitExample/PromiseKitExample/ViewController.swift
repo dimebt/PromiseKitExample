1e9249659817a93b6d89a0c981c58faa85eafa3f
Dimitar Stefanovski <dimebt_2005@hotmail.com>
2019-02-06 10:02:43 +0100

    First commit empty project

---
+++ PromiseKitExample/PromiseKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PromiseKitExample
+//
+//  Created by Dimitar Stefanovski on 2/6/19.
+//  Copyright © 2019 Dimitar Stefanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
