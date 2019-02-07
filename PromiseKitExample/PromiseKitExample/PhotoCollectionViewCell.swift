5d20b34e8c359752019d3a6c11422622f86f98d3
Dimitar Stefanovski <dimebt_2005@hotmail.com>
2019-02-07 14:15:29 +0100

    Added promise functions

---
+++ PromiseKitExample/PromiseKitExample/PhotoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Medium_02_DownloadingImages
+//
+//  Created by Dimitar Stefanovski on 2/5/19.
+//  Copyright © 2019 Dimitar Stefanovski. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+    
+}
--- PromiseKitExample/PromiseKitExample/ViewController.swift
+++ PromiseKitExample/PromiseKitExample/ViewController.swift
@@ -7,14 +7,164 @@
 //
 
 import UIKit
+import Alamofire
+import PromiseKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    //5000 photos json data
+    private let photosURL = "https://jsonplaceholder.typicode.com/photos"
+    private var photos: [UIImage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let backgroundQueue = DispatchQueue.global(qos: .background)
+        
+        firstly {
+            showLoader()
+        }.then(on: backgroundQueue) {
+            self.fetchJSON()
+        }.then(on: backgroundQueue) { (photos) in
+            self.downloadPhotos(photos: Array(photos.prefix(40)))
+        }.done(on: DispatchQueue.main, flags: nil) { _ in
+            self.hideLoader()
+            self.photoCollectionView.reloadData()
+        }.catch { (error) in
+                print(error.localizedDescription)
+        }
+        
+        
+        
     }
-
+    
+    private func showLoader() -> Guarantee<Void> {
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.startAnimating()
+        return Guarantee()
+    }
+    
+    private func hideLoader() -> Guarantee<Void> {
+        self.activityIndicator.stopAnimating()
+        return Guarantee()
+    }
+    
+    /*
+    private func downloadPhotos() {
+        //  1. Show preloading animation
+        self.activityIndicator.startAnimating()
+        //  2. Fetch and serialize json response
+        Alamofire.request(photosURL, method: .get).validate().responseData { (data) in
+            guard let data = data.result.value else { return }
+            guard let photos = try? JSONDecoder().decode([Photo].self, from: data) else { return }
+            //  3. Download photos
+            for photo in photos {
+                guard let photoUrl = URL(string: photo.url) else { return }
+                guard let photoData = try? Data(contentsOf: photoUrl) else { return }
+                guard let photoImage = UIImage(data: photoData) else { return }
+                self.photos.append(photoImage)
+            }
+            // 4. Hide preloading animation and update the UI
+            self.activityIndicator.stopAnimating()
+            self.photoCollectionView.reloadData()
+        }
+    }
+    */
+    
+    private func fetchJSON() -> Promise<[Photo]> {
+        return Promise { seal in
+            Alamofire.request(photosURL, method: .get).validate().responseData { (data) in
+                guard let data = data.result.value else {
+                    seal.reject(PhotoError.ConvertToData)
+                    return
+                }
+                guard let photos = try? JSONDecoder().decode([Photo].self, from: data) else {
+                    seal.reject(PhotoError.PhotoDecoding)
+                    return
+                }
+                seal.fulfill(photos)
+            }
+        }
+    }
+    
+    
+    
+    private func downloadPhotos(photos: [Photo]) -> Promise<[UIImage]> {
+        return Promise { seal in
+            var count = 0
+            for photo in photos {
+                guard let photoUrl = URL(string: photo.url) else {
+                    seal.reject(PhotoError.downloadPhotoUrl)
+                    return
+                }
+                guard let photoData = try? Data(contentsOf: photoUrl) else {
+                    seal.reject(PhotoError.downloadPhotoConvertToData)
+                    return
+                }
+                guard let photoImage = UIImage(data: photoData) else {
+                    seal.reject(PhotoError.downloadPhotoConvertToUIImage)
+                    return
+                }
+                self.photos.append(photoImage)
+                count+=1
+                print("Finished downloading photo: " + String(count))
+                
+            }
+            print("Finished downloading \(self.photos.count) photos")
+            seal.fulfill(self.photos)
+        }
+    }
+    
+    
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+enum PhotoError: Error {
+    case ConvertToData
+    case PhotoDecoding
+    case downloadPhotoUrl
+    case downloadPhotoConvertToData
+    case downloadPhotoConvertToUIImage
+}
+
+
+struct Photo: Codable {
+    let albumId: Int
+    let id: Int
+    let title: String
+    let url: String
+    let thumbnailUrl: String
+}
+
+
+
+
+
+
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let photoCell = self.photoCollectionView.dequeueReusableCell(withReuseIdentifier: "photo", for: indexPath) as! PhotoCollectionViewCell
+        photoCell.photo.image = self.photos[indexPath.row]
+        return photoCell
+    }
+    
+    
+}
